edb04e770e97df40d0a473271c01d1a2ad853fcf
rkhang7 <khang220300@gmail.com>
2021-08-20 20:59:14 +0700

    APPLYING MONGODB TO OUR PROJECT

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const path = require('path')
 const app = express();
 const ejs = require('ejs')
 const mongoose = require('mongoose') // connect mongodb
+const BlogPost = require('./models/BlogPost.js')
+const bodyParser =
+    require('body-parser')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true })
 
@@ -15,9 +20,13 @@ app.listen(4000, () => {
     console.log('App listening on port 4000');
 })
 
-app.get('/', (req, res) => {
+app.get('/', async(req, res) => {
     // res.sendFile(path.resolve(__dirname, 'pages/index.html'))
-    res.render('index'); // using ejs
+    const blogposts = await BlogPost.find({})
+    res.render('index', {
+        blogposts: blogposts
+    }); // using ejs
+    console.log(blogposts)
 })
 
 app.get('/about', (req, res) => {
@@ -26,6 +35,22 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
     res.render('contact'); // using ejs
 })
-app.get('/post', (req, res) => {
-    res.render('post'); // using ejs
+app.get('/post/:id', async(req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id)
+    res.render('post', {
+        blogpost
+    })
+})
+
+app.get('/posts/new', (req, res) => {
+    res.render('create')
+})
+
+app.post('/posts/store', async(req, res) => {
+    // console.log(req.body)
+    // console.log(req.body.title)
+    // console.log(req.body.body)
+    await BlogPost.create(req.body, (error, blogpost) => {
+        res.redirect('/')
+    })
 })
--- models/BlogPost.js
+++ models/BlogPost.js
@@ -4,7 +4,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema; //schema interface to represent the collections in our database
 const BlogPostSchema = new Schema({
     title: String,
-    body: String
+    body: String,
+    username: String,
+    datePosted: { /* can declare property type with an object like this because we need 'default' */
+        type: Date,
+        default: new Date()
+    }
 });
 
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema); // BlogPost is name of collection
